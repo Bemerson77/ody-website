546fb4601e42b082af7c202b1454bdfa9fe006ed
Brandon Emerson <Brandon.Emerson77@gmail.com>
2022-10-24 10:07:55 -0400

    added sass

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import HeaderLogo from '../public/ody-full-logo.png'
 import BannerLogo from '../public/ody-logo-words-01.png'
 
